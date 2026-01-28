e36c5e1876a1eb27603b6fa962bc95c18de08d6c
Alamin031 <mridoy031@gmail.com>
2026-01-28 03:50:59 -0600

    fix: comment out Testimonials component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       {/* <FreeLiveMasterclass /> */}
       <RecordedCoursesSection />
       <LiveCourseContents />
-      <Testimonials />
+      {/* <Testimonials /> */}
     </MainLayout>
   );
 }
